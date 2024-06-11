8c114aa5d2663669c33c27116ae70de5f5495df2
YuSoo <goodbyemeteor@gmail.com>
2024-06-10 22:11:41 -0700

    react set up

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
