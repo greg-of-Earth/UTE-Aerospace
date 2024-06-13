14c4e33ba05f210c71c6b8f3efaa0f9b40e5250e
YuSoo <goodbyemeteor@gmail.com>
2024-06-13 16:41:03 -0700

    add let section

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,15 @@
 import Testimonials from '../components/Testimonials';
 import WhyUteAerospace from '../components/Why' 
 import whatVideo from '../assets/what_Video.mp4'
+import Let from '../components/Let'
+
 const Home = () => {
 
   return (
     <main>
         {/* Other content */}
         <video src={whatVideo} style={{width: '100%'}}/>
+        <Let/>
         <WhyUteAerospace />
         <Testimonials />
     </main>
