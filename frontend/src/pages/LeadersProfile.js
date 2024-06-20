45d9a2e1315350f5aabcf971948de7672c39fdd9
YuSoo <goodbyemeteor@gmail.com>
2024-06-19 22:51:37 -0700

    Add ProfileCard component and LeadersProfile page

---
+++ frontend/src/pages/LeadersProfile.js
@@ -0,0 +1,15 @@
+// File: leadersProfile.js
+import React from 'react';
+import ProfileCard from '../components/ProfileCard'; // Make sure the path is correct based on your project structure
+
+function LeadersProfile() {
+  return (
+    <>
+     
+      <ProfileCard />  // This includes the Daniel Mayes profile card
+    
+    </>
+  );
+}
+
+export default LeadersProfile;
