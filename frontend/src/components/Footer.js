8dcb70a2f7bc79387f2382c7998d09275e7622d3
YuSoo <goodbyemeteor@gmail.com>
2024-06-26 22:29:55 -0700

    footer and why css

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import footerStyles from './Footer.module.css';  // Ensure this points to the CSS module
+import footerStyles from './Footer.module.css';  // Correct path assumed
 
 function Footer() {
   return (
-   
     <footer className={footerStyles.footer}>
-    
       <div className={footerStyles.footerContent}>
-        
-       
         <div className={footerStyles.footerInfo}>
           <span>Phone: 888-888-8888 | Address: </span>
         </div>
-
-       
         <div className={footerStyles.footerLinks}>
-          <a href="/privacy">Privacy Notice</a><span>|</span>  
-          <a href="/terms">Terms of Use</a><span>|</span>      
-          <a href="/cookie">Cookie Settings</a><span>|</span>  
-          <a href="/contact">Contact Us</a>                   
+          <a href="/privacy">Privacy Notice |</a>
+          <a href="/terms">Terms of Use |</a>
+          <a href="/cookie">Cookie Settings |</a>
+          <a href="/contact">Contact Us</a>
         </div>
-        
       </div>
-      
-   
       <div className={footerStyles.footerCopy}>
         Copyright © 2024 UTE Aerospace - All Rights Reserved.
       </div>
@@ -31,4 +22,4 @@ function Footer() {
   );
 }
 
-export default Footer; // Exporting the Footer component for use in other parts of the app
+export default Footer;
