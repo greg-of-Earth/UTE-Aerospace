669a01b253895434b8cf01272c7c7f10630ef5b5
greg <greg87peterson@gmail.com>
2024-06-29 02:20:12 -0700

    added title overlays

--- frontend/src/pages/CompanyLeaders.js
+++ frontend/src/pages/CompanyLeaders.js
@@ -1,5 +1,4 @@
 import co_leader from '../assets/co_leader.png';
-import no_image from '../assets/no_image.png';
 import { Link } from 'react-router-dom';
 import coLeaderStyles from './CompanyLeaders.module.css';
 import Pete from '../assets/Pete.JPG';
@@ -11,8 +10,8 @@ const CompanyLeaders = () => {
   return (
       <>
         <header className={coLeaderStyles.head}>
-        <h1 className={coLeaderStyles.headerTitle}>Company Leadership</h1>
-          <img src={co_leader} alt="company leadership" style={{width: '100%'}}></img>
+          <img className='coBackImg' src={co_leader} alt="company leadership" style={{width: '100%', maxHeight: '850px'}}></img>
+          <h1 className={coLeaderStyles.headerTitle}>Company Leadership</h1>
         </header>
         <div className={coLeaderStyles['leader-container']}>
           <figure className={coLeaderStyles['leader-card']}>
@@ -20,7 +19,8 @@ const CompanyLeaders = () => {
             width: '275px', height: '250px'}}/>
             <figcaption className={coLeaderStyles.caption}>
               <h2>Daniel Mayes</h2>
-              <span style={{display: 'block'}}>Owner/Manager</span>
+              <span style={{display: 'block', marginBottom: '10px'}}>Owner/Manager</span>
+              
               <span style={{display: 'block'}}>daniel@uteaerospace.com</span>
               <Link className={coLeaderStyles['bio-link']} to='/leaders/0'><br></br>Read Bio</Link>
             </figcaption>
@@ -31,7 +31,7 @@ const CompanyLeaders = () => {
             width: '275px', height: '250px'}}/>
             <figcaption className={coLeaderStyles.caption}>
               <h2>Pete Calderon</h2>
-              <span style={{display: 'block'}}>Co-Owner/IPT Lead</span>
+              <span style={{display: 'block', marginBottom: '10px'}}>Co-Owner/IPT Lead</span>
               <span style={{display: 'block'}}>pete@uteaerospace.com</span>
               <Link className={coLeaderStyles['bio-link']} to='/leaders/1' ><br></br>Read Bio</Link>
             </figcaption>
