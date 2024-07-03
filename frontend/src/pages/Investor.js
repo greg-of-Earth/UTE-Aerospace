dae4d3f2494bdff8a7389268d1579176c391327f
YuSoo <goodbyemeteor@gmail.com>
2024-07-02 23:02:10 -0700

    add TechnologyInnovation for investor page

--- frontend/src/pages/Investor.js
+++ frontend/src/pages/Investor.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import InvestorBanner from '../components/InvestorBanner'; 
+import TechnologyInnovation from '../components/TechnologyInnovation'
 
 function Investor() {
   return (
     <>
      
       <InvestorBanner /> 
+      <TechnologyInnovation/>
     </>
   );
 }
