962a3093923f6a2274b8bfd231c64115221d2a21
Hulk-coding <156988061+Hulk-coding@users.noreply.github.com>
2024-07-11 20:48:35 -0700

    Countdown clock bug resolved

--- frontend/src/components/UnderConstruction.js
+++ frontend/src/components/UnderConstruction.js
@@ -7,30 +7,35 @@ import HeaderTitle from '../components/HeaderTitle';
 
 const UnderConstruction = () => {
     const [timeLeft, setTimeLeft] = useState({});
-    
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         const countDownDate = new Date("Jul 22, 2024 12:00:00").getTime();
-        
+
         const updateCountdown = () => {
             const now = new Date().getTime();
             const distance = countDownDate - now;
 
-            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-            setTimeLeft({ days, hours, minutes, seconds });
-
             if (distance < 0) {
                 clearInterval(interval);
-                setTimeLeft({});
+                setTimeLeft("EXPIRED");
+            } else {
+                const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                setTimeLeft({ days, hours, minutes, seconds });
+                setLoading(false);
             }
         };
 
         const interval = setInterval(updateCountdown, 1000);
-        
-        //clears up interval
+
+        // avoid initial delay
+        updateCountdown();
+
+        // Clean up interval
         return () => clearInterval(interval);
     }, []);
     
@@ -39,9 +44,15 @@ return (
         <HeaderImg src={construction} alt="image of a computer" style={{ height: '100%', width: 'auto' }}/>
         < HeaderTitle headTitle='This Webpage is Currently Under Construction' color='white'/>
         <section id='timer' className={underConstructionStyles['timer-section']}>
-                {timeLeft.days !== undefined 
+                {/* {timeLeft.days !== undefined 
                     ? `${timeLeft.days}d ${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`
-                    : "EXPIRED"}
+                    : "EXPIRED"} */
+                    loading 
+                        ? "Loading..."
+                        : timeLeft === "EXPIRED" 
+                            ? "EXPIRED"
+                            : `${timeLeft.days}d ${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`
+                    }
         </section>
         
     </Header>
