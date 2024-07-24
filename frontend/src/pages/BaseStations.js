75135b5ff3ccfcee77eb293cc42195d42a189733
greg <greg87peterson@gmail.com>
2024-07-23 23:23:14 -0700

    added flip animation to mobile stations

--- frontend/src/pages/BaseStations.js
+++ frontend/src/pages/BaseStations.js
@@ -3,7 +3,10 @@ import HeaderImg from "../components/HeaderImg";
 import HeaderTitle from "../components/HeaderTitle";
 import baseStationImg from "../assets/baseStation.png"
 import StationTab from "../components/StationTab";
-import stationStyle from './styles/BaseStationsPage.module.css';
+
+
+
+
 
 const BaseStations = () => {
   return (
@@ -13,13 +16,13 @@ const BaseStations = () => {
         <HeaderImg src={baseStationImg}/>
     </Header>
     <div className="features-container">
-        <StationTab title='EXTENDED RANGE'> 
+        <StationTab title='EXTENDED RANGE' description='Will allow extended range and depending on the tank sizes selected will allow up to 2 days of spraying without refill'> 
         </StationTab>
         
-        <StationTab title='CAPACITY'>          
+        <StationTab title='CAPACITY' description='Will transport two drones, but when additional drones are flown to the mobile base station location, can support a fleet of up to 10 drones (based upon flying distance)'>          
         </StationTab>
 
-        <StationTab title='PERSONNEL'>
+        <StationTab title='PERSONNEL' description='Line shack for 2 people with toilet, shower, and kitchenette'>
         </StationTab>
     </div>
     </>
